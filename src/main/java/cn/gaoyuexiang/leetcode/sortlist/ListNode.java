be74e13067af1e57ad8e9715add9f4892fe18125
gaoyx <melo@gaoyuexiang.cn>
2016-07-27 17:18:41 +0800

    [#5] 完成快排

---
+++ src/main/java/cn/gaoyuexiang/leetcode/sortlist/ListNode.java
@@ -0,0 +1,7 @@
+package cn.gaoyuexiang.leetcode.sortlist;
+
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) {val = x;}
+}
---
+++ src/test/java/cn/gaoyuexiang/leetcode/sortlist/SolutionTest.java
@@ -0,0 +1,32 @@
+package cn.gaoyuexiang.leetcode.sortlist;
+
+import org.junit.Test;
+
+public class SolutionTest {
+
+	private Solution solution = new Solution();
+
+	@Test
+	public void should_sort_list() throws Exception {
+		ListNode retHead = new ListNode(1), ret = retHead;
+		for (int i = 2; i <= 10; i++) {
+			ret.next = new ListNode(i);
+			ret = ret.next;
+		}
+
+		ListNode unSortHead = new ListNode(10), unSort = unSortHead;
+		for (int i = 9; i > 0; i--) {
+			unSort.next = new ListNode(i);
+			unSort = unSort.next;
+		}
+
+		ListNode sortHead = solution.sortList(unSortHead);
+
+		while (sortHead != null) {
+			assert sortHead.val == retHead.val;
+			sortHead = sortHead.next;
+			retHead = retHead.next;
+		}
+
+	}
+}
