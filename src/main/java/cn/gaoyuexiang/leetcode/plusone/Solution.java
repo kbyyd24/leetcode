4afc08a779965276a6812457d031aa19fbadb34c
gaoyx <melo@gaoyuexiang.cn>
2016-07-27 14:37:51 +0800

    [#3] 通过Plus One Problem

---
+++ src/main/java/cn/gaoyuexiang/leetcode/plusone/Solution.java
@@ -0,0 +1,40 @@
+package cn.gaoyuexiang.leetcode.plusone;
+
+import java.util.ArrayList;
+
+/**
+ issues: #3
+
+ Given a non-negative number represented as an array of digits, plus one to the number.
+
+ The digits are stored such that the most significant digit is at the head of the list.
+
+ */
+
+public class Solution {
+	public int[] plusOne(int[] digits) {
+		if (digits == null) return null;
+		int length = digits.length;
+		digits[length - 1] ++;
+		for (int i = length - 1; i > 0; i--) {
+			digits[i - 1] += digits[i] / 10;
+			digits[i] %= 10;
+		}
+		if (digits[0] > 9) {
+			ArrayList<Integer> upNums = new ArrayList<>();
+			while (digits[0] > 9) {
+				upNums.add(digits[0] % 10);
+				digits[0] /= 10;
+			}
+			int size = upNums.size();
+			int[] ret = new int[length + size];
+			ret[0] = digits[0];
+			for (int i = 1; i < ret.length; i++) {
+				if (i <= size) ret[i] = upNums.get(size - i);
+				else ret[i] = digits[i - size];
+			}
+			return ret;
+		}
+		return digits;
+	}
+}
---
+++ src/test/java/cn/gaoyuexiang/leetcode/plusone/SolutionTest.java
@@ -0,0 +1,31 @@
+package cn.gaoyuexiang.leetcode.plusone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class SolutionTest {
+
+	private Solution solution = new Solution();
+
+	@Test
+	public void get_right_result() throws Exception {
+		int[] digits = {1, 23, 234, 62};
+		int[] result = solution.plusOne(digits);
+		assertArrayEquals(result, new int[]{5, 7, 0, 3});
+	}
+
+	@Test
+	public void name() throws Exception {
+		int[] digits = {7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,6};
+		int[] result = solution.plusOne(digits);
+		assertArrayEquals(result, new int[]{7,2,8,5,0,9,1,2,9,5,3,6,6,7,3,2,8,4,3,7,9,5,7,7,4,7,4,9,4,7,0,1,1,1,7,4,0,0,7});
+	}
+
+	@Test
+	public void should_up_number() throws Exception {
+		int[] digits = {9};
+		int[] result = solution.plusOne(digits);
+		assertArrayEquals(result, new int[]{1,0});
+	}
+}
