0c68d2a46d061545910abda9214388570bbdaace
gaoyx <melo@gaoyuexiang.cn>
2016-08-03 15:48:31 +0800

    [#18] 优化,减少交换次数

--- src/main/java/cn/gaoyuexiang/leetcode/sort/bubble/moveZeores/Solution.java
+++ src/main/java/cn/gaoyuexiang/leetcode/sort/bubble/moveZeores/Solution.java
@@ -8,21 +8,12 @@ package cn.gaoyuexiang.leetcode.sort.bubble.moveZeores;
 
 public class Solution {
 	public void moveZeroes(int[] nums) {
-		int lastNonZero = -1;
-		for (int i = nums.length - 1; i >= 0; i--) {
-			if (nums[i] != 0 && lastNonZero == -1) lastNonZero = i;
-			else if (nums[i] == 0 && lastNonZero != -1) {
-				sort(nums, i, lastNonZero);
-				lastNonZero--;
+		for (int firstZero = 0, i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				int temp = nums[firstZero];
+				nums[firstZero++] = nums[i];
+				nums[i] = temp;
 			}
-		}
-	}
-
-	private void sort(int[] nums, int start, int end) {
-		for (int i = start; i < end; i++) {
-			int temp = nums[i];
-			nums[i] = nums[i + 1];
-			nums[i + 1] = temp;
 		}
 	}
 }
