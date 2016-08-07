6a6d0676a89b5d36946d624308a39edade077b08
gaoyx <melo@gaoyuexiang.cn>
2016-08-08 07:08:57 +0800

    one-pass

--- src/main/java/cn/gaoyuexiang/leetcode/array/twoSum/Solution.java
+++ src/main/java/cn/gaoyuexiang/leetcode/array/twoSum/Solution.java
@@ -1,5 +1,7 @@
 package cn.gaoyuexiang.leetcode.array.twoSum;
 
+import java.util.HashMap;
+
 /**
  *
  * issues : #20
@@ -9,14 +11,13 @@ public class Solution {
   public int[] twoSum(int[] nums, int target) {
     if (nums == null) return null;
     if (nums.length < 2) return nums;
-    int length = nums.length;
-    for (int i = 0; i < length - 1; i++) {
+    HashMap<Integer, Integer> map = new HashMap<>();
+    for (int i = 0; i < nums.length; i++) {
       int diff = target - nums[i];
-      for (int j = i + 1; j < length; j++) {
-        if (diff == nums[j])
-          return new int[]{i, j};
-      }
+      if (map.containsKey(diff))
+        return new int[]{map.get(diff), i};
+      map.put(nums[i], i);
     }
-    return new int[0];
+    return null;
   }
 }
