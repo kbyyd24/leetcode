d761b43159362b79f1995c9b8a5ba2e8fe2ec5be
gaoyx <melo@gaoyuexiang.cn>
2016-08-08 11:30:26 +0800

    [#22] 优化效率

--- src/main/java/cn/gaoyuexiang/leetcode/string/substring/longestSubstringWithoutRepeatingCharacters/Solution.java
+++ src/main/java/cn/gaoyuexiang/leetcode/string/substring/longestSubstringWithoutRepeatingCharacters/Solution.java
@@ -1,6 +1,7 @@
 package cn.gaoyuexiang.leetcode.string.substring.longestSubstringWithoutRepeatingCharacters;
 
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * issues : #22
@@ -8,21 +9,16 @@ import java.util.HashMap;
 
 public class Solution {
   public int lengthOfLongestSubstring(String s) {
-    char[] chars = s.toCharArray();
-    int ret = 0, cur = 0;
+    int len = s.length(), ret = 0;
     HashMap<Character, Integer> charMap = new HashMap<>();
-    for (int i = 0; i < chars.length; i++) {
-      char item = chars[i];
+    for (int i = 0, j = 0; i < len; i++) {
+      char item = s.charAt(i);
       if (charMap.containsKey(item)) {
-        ret = cur > ret ? cur : ret;
-        i = charMap.get(item);
-        charMap.clear();
-        cur = 0;
-      } else {
-        charMap.put(item, i);
-        cur ++;
+        j = Math.max(charMap.get(item), j);
       }
+      ret = Math.max(ret, i - j + 1);
+      charMap.put(item, i + 1);
     }
-    return ret >= cur ? ret : cur;
+    return ret;
   }
 }
